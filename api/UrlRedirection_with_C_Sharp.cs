470913887e471eed30c72fa3689520b081639631
ndissan <68413948+ndissan@users.noreply.github.com>
2021-11-15 18:20:29 +1100

    updated the code - successful with testing

--- api/UrlRedirection_with_C_Sharp.cs
+++ api/UrlRedirection_with_C_Sharp.cs
@@ -19,19 +19,6 @@ namespace Company.Function
             HttpRequest req,
             ILogger log)
         {
-            //log.LogInformation("C# HTTP trigger function processed a request.");
-
-            //string name = req.Query["name"];
-
-            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-           // dynamic data = JsonConvert.DeserializeObject(requestBody);
-            //name = name ?? data?.name;
-
-           // string responseMessage = string.IsNullOrEmpty(name)
-           //     ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-           //     : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            //return new OkObjectResult(responseMessage);
             return new RedirectResult("https://www.gs1au.org/nlr", true);
         }
     }
