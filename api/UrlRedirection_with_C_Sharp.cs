cf408d094c82ca36c2b6fa77f5009700b092864d
ndissan <68413948+ndissan@users.noreply.github.com>
2021-11-15 17:23:30 +1100

    updated c# code for URL redirection

--- api/UrlRedirection_with_C_Sharp.cs
+++ api/UrlRedirection_with_C_Sharp.cs
@@ -13,9 +13,25 @@ namespace Company.Function
     public static class UrlRedirection_with_C_Sharp
     {
 
-        [FunctionName("UrlRedirection_with_C_Sharp")]
-        public static IActionResult Run(HttpRequest req, ILogger log)
+        [FunctionName("UrlRedirection01")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] 
+            HttpRequest req,
+            ILogger log)
         {
+            //log.LogInformation("C# HTTP trigger function processed a request.");
+
+            //string name = req.Query["name"];
+
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+           // dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
+
+           // string responseMessage = string.IsNullOrEmpty(name)
+           //     ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+           //     : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            //return new OkObjectResult(responseMessage);
             return new RedirectResult("https://www.gs1au.org/nlr", true);
         }
     }
